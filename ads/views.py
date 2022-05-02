e606724a62e1ba7332306c7b17a71ba9ec2aa512
Bjarki <bjarkis21@ru.is>
2022-05-02 15:41:38 +0000

    setting up new apps

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+adv = [
+    {'id':1, 'title':'First ad', 'price':'$100'},
+    {'id':2, 'title':'Second ad', 'price':'$200'},
+    {'id':3, 'title':'Third ad', 'price':'$300'},
+]
+
+def advertisements(request):
+    context = {'adv':adv}
+    return render(request, 'ads.html', context)
--- firesale/urls.py
+++ firesale/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('', include('ads.urls')),
+    #path('', include('user.urls'))
 ]
 
