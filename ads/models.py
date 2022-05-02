6f5f54ba0bb00cb3a9c400a4673a74e85ad8b77e
Bjarki <bjarkis21@ru.is>
2022-05-02 22:48:24 +0000

    make migrations

--- ads/models.py
+++ ads/models.py
@@ -1,5 +1,45 @@
 from django.db import models
+from user.models import UserProfile
+
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=255)
+    short_description = models.CharField(max_length=255)
+    long_description = models.CharField(max_length=9999)
+    condition = models.CharField(max_length=255)
+    image = models.CharField(max_length=500, blank=True)
+    reserve = models.IntegerField(blank=True, null=True)
+    seller = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='seller')
+    creation_date = models.DateTimeField()
+    buyer = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, blank=True, related_name='buyer', null=True)
+    rating = models.FloatField(blank=True, null=True)
+    buy_date = models.DateTimeField(blank=True, null=True)
+    isPaid = models.BooleanField()
+    isSold = models.BooleanField()
+
+    def __str__(self):
+        return self.title
+
+
+class BidsOn(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+    amount = models.IntegerField()
+    bid_date = models.DateTimeField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'advertisement', 'amount'], name='keyConstraint')
+        ]
+
+    def __str__(self):
+        return f"{self.user.name}, {self.advertisement.title}, {str(self.amount)}"
--- user/models.py
+++ user/models.py
@@ -9,18 +9,24 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     street_no = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.street}, {self.street_no}"
+
 
 class UserProfile(models.Model):
     name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    #email = models.CharField(max_length=255)
     phone = models.CharField(max_length=30, blank=True)
-    rating = models.FloatField(blank=True)
+    rating = models.FloatField(blank=True, null=True)
     gender = models.CharField(max_length=10, blank=True)
     description = models.CharField(max_length=9999, blank=True)
     ssn = models.CharField(max_length=10, blank=True)
     bank = models.CharField(max_length=10, blank=True)
     hb = models.CharField(max_length=10, blank=True)
     account_no = models.CharField(max_length=20, blank=True)
-    address = models.ForeignKey(Address, on_delete='SET_NULL', blank=True)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
 
