0054b76d4028e4a7edbfa3d36a4145dfcb0e9e54
viktoriarb <viktoriarb20@ru.is>
2022-05-04 10:49:39 +0000

    laga

--- user/urls.py
+++ user/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from user import views
 
 urlpatterns = [
-    path('register', views.register, name='Nýskrá'),
-    path('login', LoginView.as_view(template_name='user/login.html'), name='Innskrá'),
-    path('logout', LogoutView.as_view(next_page='login'), name='Útskrá'),
+    path('register', views.register, name='register'),
+    path('login', LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name='logout'),
     path('profile', views.profile, name='profile'),
     path('bankinfo', views.bank_info, name='bank-info')
 ]
