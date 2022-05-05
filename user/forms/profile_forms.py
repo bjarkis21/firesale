0cf896340cb3c9367ea8c37ad5bc546200c040f4
Bjarki <bjarkis21@ru.is>
2022-05-05 23:35:01 +0000

    add email

--- user/forms/profile_forms.py
+++ user/forms/profile_forms.py
@@ -1,6 +1,15 @@
-from django.forms import ModelForm, widgets
-
+from django.forms import ModelForm, widgets, EmailField
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
 from user.models import UserProfile
+
+class CustomUserForm(UserCreationForm):
+    email = EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
 class ProfileForm(ModelForm):
     class Meta:
