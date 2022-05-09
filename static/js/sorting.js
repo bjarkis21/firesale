a060687f43e42d71ddf693a44326774d6227053b
Bjarki <bjarkis21@ru.is>
2022-05-09 22:48:23 +0000

    can sort

--- static/js/sorting.js
+++ static/js/sorting.js
@@ -19,6 +19,29 @@ function sort_by_title(ads_container) {
     return ads_array
 }
 
+function sort_by_date(ads_container) {
+    let ads_array = [];
+    for (let ad of ads_container) {
+        ads_array.push(ad)
+    }
+    ads_array.sort(function(a,b){
+        let time_a = a.querySelector(".ad-creation-date").dataset.timestamp;
+        time_a = Number(time_a)
+        time_a = new Date(time_a)
+        let time_b = b.querySelector(".ad-creation-date").dataset.timestamp;
+        time_b = Number(time_b)
+        time_b = new Date(time_b)
+        if (time_a < time_b){
+            return -1
+        }else if (time_a > time_b){
+            return 1
+        }else {
+            return 0
+        }
+    })
+    return ads_array
+}
+
 document.querySelector("#sort_by_name_asc").addEventListener('click', function(){
     let ads_container = document.getElementsByClassName('ad-anchor1');
     let ads_array = sort_by_title(ads_container)
@@ -38,3 +61,28 @@ document.querySelector("#sort_by_name_desc").addEventListener('click', function(
         ads_container_element.appendChild(ad)
     }
 })
+
+document.querySelector("#sort_by_date_asc").addEventListener('click', function(){
+    let ads_container = document.getElementsByClassName('ad-anchor1');
+    let ads_array = sort_by_date(ads_container)
+    let ads_container_element = document.querySelector(".ads-container")
+    ads_container_element.innerHTML = ""
+    for (let ad of ads_array) {
+        ads_container_element.appendChild(ad)
+    }
+    console.log("hello")
+
+})
+
+document.querySelector("#sort_by_date_desc").addEventListener('click', function(){
+    let ads_container = document.getElementsByClassName('ad-anchor1');
+    let ads_array = sort_by_date(ads_container).reverse()
+    let ads_container_element = document.querySelector(".ads-container")
+    ads_container_element.innerHTML = ""
+    for (let ad of ads_array) {
+        ads_container_element.appendChild(ad)
+    }
+    console.log("hello")
+
+})
+
