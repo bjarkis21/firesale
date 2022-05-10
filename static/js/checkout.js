10e90936180f849ea65e7fbfe995dcb88ca81e21
Bjarki <bjarkis21@ru.is>
2022-05-10 15:23:41 +0000

    change

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -4,11 +4,12 @@ let address_info_elements = document.getElementsByClassName("address-info");
 for (let elem of address_info_elements) {
     let parent = elem.parentNode;
     parent.classList.add("address-info-container");
-    parent.classList.add("checkout-active")
 }
 
 let cc_info_elements = document.getElementsByClassName("cc-info");
 for (let elem of cc_info_elements) {
     let parent = elem.parentNode;
     parent.classList.add("cc-info-container");
+    parent.classList.add("checkout-hidden");
 }
+
