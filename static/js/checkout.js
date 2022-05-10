d438cafeca43e3b6b5a9e02b30c461070dda4f85
Bjarki <bjarkis21@ru.is>
2022-05-10 15:35:17 +0000

    change

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,5 +1,7 @@
 "use strict"
 
+let page_no = 1
+
 let address_info_elements = document.getElementsByClassName("address-info");
 for (let elem of address_info_elements) {
     let parent = elem.parentNode;
@@ -13,3 +15,37 @@ for (let elem of cc_info_elements) {
     parent.classList.add("checkout-hidden");
 }
 
+function toggleCheckout() {
+    for (let elem of address_info_elements) {
+            let parent = elem.parentNode;
+            parent.classList.toggle("checkout-hidden")
+        }
+        for (let elem of cc_info_elements) {
+            let parent = elem.parentNode;
+            parent.classList.toggle("checkout-hidden")
+        }
+}
+
+document.querySelector(".checkout-continue-btn").addEventListener('click', function() {
+    if (page_no === 1) {
+        toggleCheckout()
+        document.querySelector(".checkout-back-btn").classList.toggle("checkout-visibility-hidden");
+        page_no += 1
+    }else if (page_no === 2) {
+
+    }else if (page_no === 3) {
+
+    }
+})
+
+document.querySelector(".checkout-back-btn").addEventListener('click', function() {
+    if (page_no === 1) {
+
+    }else if (page_no === 2) {
+        toggleCheckout()
+        document.querySelector(".checkout-back-btn").classList.toggle("checkout-visibility-hidden");
+        page_no -= 1
+    }else if (page_no === 3) {
+
+    }
+})
