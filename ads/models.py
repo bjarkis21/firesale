6e286976277784a07e6e50032e642c2582aa5dfa
Bjarki <bjarkis21@ru.is>
2022-05-10 12:32:02 +0000

    change

--- ads/models.py
+++ ads/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
-from user.models import UserProfile
+#from user.models import UserProfile
 from datetime import datetime
 
 
