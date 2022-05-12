1034fa0befa1495ef2ec0924e1338c2630eed271
Ísak Maron <isakh21@ru.is>
2022-05-12 12:31:43 +0000

    fix give rating

--- static/js/rating.js
+++ static/js/rating.js
@@ -1,4 +1,5 @@
 $("star-rating").click(function() {
+    $(this).parent().find("star-rating").css({"color" : "black"});
     $(this).css({"color" : "orange"});
     $(this).nextAll().css({"color" : "orange"});
 });
